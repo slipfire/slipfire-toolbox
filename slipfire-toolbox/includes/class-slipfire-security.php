065720460209f3dfe90a7b05426990fba25e073d
Steven Bruner <sbruner@slipfire.com>
2015-11-11 23:54:00 -0500

    debug.log check

---
+++ slipfire-toolbox/includes/class-slipfire-security.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!defined('ABSPATH'))
+{
+  exit;
+}
+
+$slipfire_security = new SlipFire_Security();
+
+class SlipFire_Security
+{
+  public function __construct()
+  {
+  	add_action('slipfire_hourly_cron_job', array('slipfire_security', 'test_debug_log'));
+	}
+
+	/**
+	 * If debug.log is publically accessible email me.
+	 */
+	public static function test_debug_log()
+	{
+		$file = 'debug.log';
+		$response = wp_remote_get(content_url($file));
+		$header = wp_remote_retrieve_response_code($response);
+
+		if($header != '404')
+		{
+			$to = 'sbruner@slipfire.com';
+			$subject = 'IMPORTANT: debug.log is Public';
+			$message = 'File is public ' . content_url($file);
+			mail($to, $subject, $message);
+		}
+	}
+}
