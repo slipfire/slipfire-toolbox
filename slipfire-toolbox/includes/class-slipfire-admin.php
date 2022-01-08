8dda3e7b2635e315795e4f3218e5d2ab34fb36bb
Steve Bruner <sbruner@slipfire.com>
2022-01-08 18:38:14 -0500

    remove dashboard widgets, admin footer text, jetpack filter

--- slipfire-toolbox/includes/class-slipfire-admin.php
+++ slipfire-toolbox/includes/class-slipfire-admin.php
@@ -12,7 +12,38 @@ class SlipFire_Admin {
 	 * Initialize the class
 	 */
 	public function __construct() {
+		add_action( 'init', array( 'slipfire_admin', 'init' ) );
+		add_action( 'wp_dashboard_setup', array( 'slipfire_admin', 'remove_dashboard_widgets' ) );
 		add_filter( 'admin_body_class', array( 'slipfire_admin', 'admin_body_class' ) );
+		add_filter( 'admin_footer_text', array( 'slipfire_admin', 'footer_text' ) );
+	}
+
+	/**
+	 * Run on init
+	 *
+	 * @var String
+	 * @since 1.0.0
+	 */
+	public static function init() {
+		self::jetpack();
+	}
+
+	/**
+	 * Remove some core dashboard widgets
+	 *
+	 * @var String
+	 * @since 4.9.19
+	 */
+	public static function remove_dashboard_widgets() {
+		remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );   // Right Now
+		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' ); // Recent Comments
+		remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );  // Incoming Links
+		remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );   // Plugins
+		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );  // Quick Press
+		remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );  // Recent Drafts
+		remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );   // WordPress blog
+		remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );   // Other WordPress News
+		remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' ); // Activity
 	}
 
 	/**
@@ -25,4 +56,29 @@ class SlipFire_Admin {
 
 		return $classes;
 	}
+
+	/**
+	 * Update footer text in admin
+	 *
+	 * @var String
+	 * @since 4.9.19
+	 */
+	public static function footer_text() {
+		echo 'Built for you by <a href="https://slipfire.com">SlipFire</a>';
+	}
+
+	/**
+	 * Jetpack customizations
+	 *
+	 * @var String
+	 * @since 4.9.19
+	 */
+	public static function jetpack() {
+		add_filter( 'jetpack_just_in_time_msgs', '__return_false' );
+	}
+
+
+
+
+
 }
